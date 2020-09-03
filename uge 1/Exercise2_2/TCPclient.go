b40942e66958b15417eb2ca087b6a6c23120f882
Anders Holst <anders.holst.rasmussen@gmail.com>
2020-09-03 15:26:18 +0200

    Done for today, things does not work

---
+++ uge 1/Exercise2_2/TCPclient.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+var conn net.Conn
+var ipstr string
+
+func main() {
+	conn, _ := net.Dial("tcp", "127.0.0.1:18081")
+	defer conn.Close()
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("> ")
+		text, err := reader.ReadString('\n')
+		if text == "quit\n" {
+			return
+		}
+		fmt.Fprint(conn, text)
+		msg, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			return
+		}
+		fmt.Print("From server: " + msg)
+	}
+}
